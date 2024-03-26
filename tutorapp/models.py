e7190b5a00452c11080e2ba436692b37765a9040
akhil989 <mmakhil989@gmail.com>
2024-03-27 00:01:11 +0530

    Rating added in models.py

--- tutorapp/models.py
+++ tutorapp/models.py
@@ -11,7 +11,11 @@ class Category(models.Model):
 
     def __str__(self)->str:
         return self.name
+class Ratings(models.Model):
+    rating = models.DecimalField(max_digits=2, decimal_places=1) 
     
+    def __str__(self) -> str:
+        return self.rating
 class VideoModel(models.Model):
     title = models.CharField(max_length=200)
     thumbnail = models.ImageField(null=False, upload_to='thumbnails', default='')
@@ -21,6 +25,7 @@ class VideoModel(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    rating = models.ForeignKey(Ratings, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
